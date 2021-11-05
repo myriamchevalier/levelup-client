fa62c500b75f2fc85366952dace2589edcc18808
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-11-05 09:06:25 -0500

    game form implemented

--- src/components/games/GameForm.js
+++ src/components/games/GameForm.js
@@ -94,8 +94,6 @@ export const GameForm = () => {
                 </div>
             </fieldset>
 
-            {/* TODO: create the rest of the input fields */}
-
             <button type="submit"
                 onClick={evt => {
                     // Prevent form from being submitted
