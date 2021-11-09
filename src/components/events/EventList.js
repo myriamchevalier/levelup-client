2aaf61b2323d43254c4d6933375730c412bbecdb
Myriam Chevalier <chevalier.myriam@hotmail.com>
2021-11-09 09:15:43 -0600

    added codee to allow users to join event, added links in navbar

--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -1,30 +1,50 @@
-import React, { useState, useEffect } from "react"
-import { getEvents } from "./EventManager.js"
+import React, { useEffect, useState} from "react"
+import { useHistory } from "react-router-dom"
+import { getEvents, joinEvent } from "./EventManager.js"
 
-export const EventList = (props) => {
-    const [events, setEvents] = useState([])
+
+export const EventList = () => {
+    const history = useHistory()
+    const [ events, assignEvents ] = useState([])
+
+    const eventFetcher = () => {
+        getEvents()
+            .then(data => assignEvents(data))
+    }
 
     useEffect(() => {
-        getEvents().then(data => setEvents(data))
+        eventFetcher()
     }, [])
 
     return (
         <article className="events">
+            <header className="events__header">
+                <h1>Level Up Game Events</h1>
+                <button className="btn btn-2 btn-sep icon-create"
+                    onClick={() => {
+                        history.push({ pathname: "/events/new" })
+                    }}
+                >Schedule New Event</button>
+            </header>
             {
                 events.map(event => {
-                    return <section key={`event--${event.id}`} className="event">
-                        <div className="event__game">Game : {event?.game?.title}</div>
-                        <div className="event__description">Description: {event.description}</div>
-                        <div className="event_datetime">When: {event.date} at {event.time}</div>
-                        <div className="event__organizer">Organized by: {event?.organizer?.user?.first_name} {event?.organizer?.user?.last_name}</div>
+                    return <section key={event.id} className="registration">
+                        <div className="registration__game">{event.game.title}</div>
+                        <div>{event.description}</div>
+                        <div>
+                            {event.date} @ {event.time}
+                        </div>
+                        <button className="btn btn-2"
+                                onClick={
+                                    () => {
+                                        joinEvent(event.id)
+                                            .then(() => eventFetcher())
+                                    }
+                                }
+                        >Join</button>
                     </section>
                 })
             }
-            <button className="btn btn-2 btn-sep icon-create"
-                onClick={() => {
-                    history.push({ pathname: "/events/new" })
-                }}
-            >Create New Event</button>
-        </article>
+        </article >
     )
 }
--- src/components/events/EventManager.js
+++ src/components/events/EventManager.js
@@ -21,3 +21,14 @@ export const createEvent = (event) => {
      })
      .then(getEvents)
 }
+
+export const joinEvent = eventId => {
+    return fetch(`http://localhost:8000/events/${ eventId }/signup`, {
+        method: "POST",
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+        .then(response => response.json())
+}
+
